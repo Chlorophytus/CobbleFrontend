4170c0a7f52e646e951870242226a2f611957d14
Roland Metivier <metivier.roland@chlorophyt.us>
2025-08-09 20:59:02 -0500

    add more media query dependent layout stuff
    so it'll fit on my iPhone

--- src/app.js
+++ src/app.js
@@ -5,27 +5,27 @@ window.Alpine = Alpine
 Alpine.start()
 
 function initializeVideos() {
-    return [...Array(12).keys()].map((index) => {
+    return [...Array(16).keys()].map((index) => {
         let newVideo = document.createElement("div")
-        newVideo.className = "bg-white w-96 h-88 m-8 rounded-xl shadow-lg"
+        newVideo.className = "xl:m-8 xl:w-96 xl:h-88 bg-white w-70 h-70 m-2 rounded-xl shadow-lg"
 
         let newThumb = document.createElement("div")
-        newThumb.className = "rounded-xl shadow-lg w-80 h-45 m-8 bg-zinc-100"
+        newThumb.className = "xl:w-80 xl:h-45 xl:m-8 rounded-xl w-56 h-32 shadow-lg m-6 bg-zinc-100"
         newVideo.appendChild(newThumb)
 
         let newTitle = document.createElement("h3")
         newTitle.innerText = `Test video ${index}`
-        newTitle.className = "text-2xl px-10"
+        newTitle.className = "xl:text-2xl text-xl px-10"
         newVideo.appendChild(newTitle)
 
         let newUploadUser = document.createElement("p")
         newUploadUser.innerText = `by test`
-        newUploadUser.className = "px-10"
+        newUploadUser.className = "xl:text-lg text-sm px-10"
         newVideo.appendChild(newUploadUser)
 
         let newUploadDate = document.createElement("p")
         newUploadDate.innerText = `on test`
-        newUploadDate.className = "px-10"
+        newUploadDate.className = "xl:text-lg text-sm px-10"
         newVideo.appendChild(newUploadDate)
 
         return newVideo;
