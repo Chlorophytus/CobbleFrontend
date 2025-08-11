0225706d9ae08e8219672a4c400b4401b0d91a65
Roland Metivier <metivier.roland@chlorophyt.us>
2025-08-11 15:09:10 -0500

    add load timer

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ The frontend for Cobble
 
 ## Developing
 This is a Vite-based web app, but you can do live development when you
-connect it to the backend API. TODO: Document this.
+connect it to the backend API.
 
 ```shell
 $ npm run dev
--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ window.Alpine = Alpine
 
 Alpine.start()
 
-const API_ENDPOINT = "http://127.0.0.1:8080/"
+const API_ENDPOINT = "http://192.168.88.10:8080/"
 
-function initializeVideos() {
-    return [...Array(8).keys()].map((index) => {
+function initializeVideos(videos) {
+    return videos.map((video) => {
         let newVideo = document.createElement("div")
         newVideo.className = "xl:m-8 xl:w-96 xl:h-88 bg-white w-70 h-70 m-2 rounded-xl shadow-lg"
 
@@ -16,17 +16,17 @@ function initializeVideos() {
         newVideo.appendChild(newThumb)
 
         let newTitle = document.createElement("h3")
-        newTitle.innerText = `Test video ${index}`
+        newTitle.innerText = video.title
         newTitle.className = "xl:text-2xl text-xl px-10"
         newVideo.appendChild(newTitle)
 
         let newUploadUser = document.createElement("p")
-        newUploadUser.innerText = `by test`
+        newUploadUser.innerText = `by ${video.uploader}`
         newUploadUser.className = "xl:text-lg text-sm px-10"
         newVideo.appendChild(newUploadUser)
 
         let newUploadDate = document.createElement("p")
-        newUploadDate.innerText = `on test`
+        newUploadDate.innerText = `on ${video.date}`
         newUploadDate.className = "xl:text-lg text-sm px-10"
         newVideo.appendChild(newUploadDate)
 
@@ -34,13 +34,15 @@ function initializeVideos() {
     })
 }
 
-function getVersion() {
-    fetch(new URL("/.well-known/version", API_ENDPOINT)).then((result) => {
+function initializePage() {
+    fetch(new URL("/page", API_ENDPOINT)).then((result) => {
         return result.json()
-    }).then((json) =>
-        document.getElementById("version").innerText = `Backend v${json.humanReadable}`
-    )
+    }).then((json) => {
+        if (json.ok) {
+            initializeVideos(json.videos).forEach((video) => { document.getElementById("videos").appendChild(video) })
+            document.getElementById("version").innerText = `Backend v${json.version.readable} | Handled in ${json.responseTime}ms`
+        }
+    })
 }
 
-initializeVideos().forEach((video) => { document.getElementById("videos").appendChild(video) })
-getVersion()
+initializePage()
